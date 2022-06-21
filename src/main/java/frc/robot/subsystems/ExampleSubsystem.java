37557c8e1e748cdadcbff0cf3e37a151059cb4ee
Adam S <majestix123@outlook.com>
2022-06-21 10:06:14 -0500

    update checkstyle to 4 space indents

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -10,16 +10,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * A blank subsystem created by WPILib.
  */
 public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+    /** Creates a new ExampleSubsystem. */
+    public ExampleSubsystem() {
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+    }
 }
